1a43dcba7401c1a13bc3ed753a67287e84e0feb2
NinaEli <miyassar818@outlook.com>
2019-01-29 15:47:39 -0500

    add new message to git

--- src/test/java/HomeWork/Practice.java
+++ src/test/java/HomeWork/Practice.java
@@ -1,7 +1,13 @@
 package HomeWork;
 
+import javax.jws.soap.SOAPBinding;
+
 public class Practice {
     public static void main(String[] args) {
         System.out.println("Snowing");
+
+        String word="ICECREAM";
+        System.out.println(word.length();
+        System.out.println(word.charAt(4));
     }
 }
